e69f5992bacea629ba010e81530a24fb709f3969
Abdullah <ranamuhammadabdullah1122@gmail.com>
2026-03-06 10:16:00 +0500

    Prepare for Vercel deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,25 +7,37 @@ import Alert from "./components/Alert";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
-
 function App() {
-  console.log("App component rendered"); 
+  console.log("App component rendered");
+
   return (
-      <NoteState>
-        <Router>
-          <Navbar />
-          <Alert message="This is amazing react app"/>
-          <div className="container">
+    <NoteState>
+      <Router>
+        <Navbar />
+        <Alert message="This is an amazing React app" />
+        <div className="container">
           <Routes>
+            {/* Root/home route */}
             <Route path="/" element={<Home />} />
-            
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Signup" element={<Signup />} />
+
+            {/* ✅ Use lowercase paths to avoid 404 */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+
+            {/* Optional: Catch-all route for unknown URLs */}
+            <Route
+              path="*"
+              element={
+                <div className="text-center mt-5">
+                  <h2>404 - Page Not Found</h2>
+                  <p>The page you are looking for does not exist.</p>
+                </div>
+              }
+            />
           </Routes>
-          </div>
-        </Router>
-      </NoteState>
-    
+        </div>
+      </Router>
+    </NoteState>
   );
 }
 
